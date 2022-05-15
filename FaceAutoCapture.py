640b40a26630ba519c406387495e5518b049dd74
AhmedAhmedEG <ahmed.ahmed.a.m.aa12@gmail.com>
2022-05-15 07:17:09 +0200

    Update
    Now multiproccesing for audio
    Custom edit for CVZone got added with the project

--- FaceAutoCapture.py
+++ FaceAutoCapture.py
@@ -1,68 +1,82 @@
-from cvzone.FaceDetectionModule import FaceDetector
+from FaceDetectionModule import FaceDetector
+from multiprocessing import Process
 from datetime import datetime
 from time import sleep
+import RPi.GPIO as GPIO
 import vlc
 import cv2
+import sys
 import os
 
-if not os.path.isdir("Captures"):
-    os.mkdir("Captures")
 
-dir_name = datetime.now().strftime("%d.%m.%Y.%H.%M.%S")
-os.mkdir(os.path.join("Captures", dir_name))
+def play_sound(name, d):
+    sound = vlc.MediaPlayer(f"{name}.mp3")
+    sound.play()
+    sleep(d)
+    sound.stop()
+    sys.exit(0)
 
-cap = cv2.VideoCapture(1)
-cap.set(cv2.CAP_PROP_BUFFERSIZE, 0)
 
-detector = FaceDetector()
-found = vlc.MediaPlayer("found.mp3")
-lost = vlc.MediaPlayer("lost.mp3")
+if __name__ == "__main__":
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.add_event_detect(4, GPIO.RISING, bouncetime=250)
 
-faces_number = 0
-min_score = 0.8
-c = 0
+    path = "Captures"
+    if not os.path.isdir(path):
+        os.mkdir(path)
 
-while True:
-    _, img = cap.read()
-    img, bboxs = detector.findFaces(img)
+    dir_name = datetime.now().strftime("%d.%m.%Y.%H.%M.%S")
+    os.mkdir(os.path.join(path, dir_name))
 
-    if len(bboxs) > faces_number:
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
 
-        for f in bboxs[faces_number - 1:]:
+    detector = FaceDetector()
 
-            if f["score"] > min_score:
-                faces_number += 1
+    faces_number = 0
+    min_score = 0.8
+    found_duration = 2
+    lost_duration = 2
+    c = 0
 
-                found.play()
-                sleep(0.5)
-                found.stop()
+    while True:
+        _, img = cap.read()
+        img, bboxs = detector.findFaces(img)
 
-                cv2.imwrite(os.path.join("Captures", dir_name,f"{c}.png"), img)
-                c += 1
+        if GPIO.event_detected(4):
+            cv2.imwrite(os.path.join(path, dir_name, f"{c}.png"), img)
+            c += 1
 
-    elif len(bboxs) < faces_number:
-
-        if len(bboxs) == 0:
-            faces_number -= 1
-
-            lost.play()
-            sleep(0.5)
-            lost.stop()
-
-        else:
+        if len(bboxs) > faces_number:
 
             for f in bboxs[faces_number - 1:]:
 
                 if f["score"] > min_score:
-                    faces_number -= 1
+                    faces_number += 1
 
-                    lost.play()
-                    sleep(0.5)
-                    lost.stop()
+                    Process(target=play_sound, args=("found", found_duration, )).start()
 
-    cv2.imshow("Image", img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        elif len(bboxs) < faces_number:
 
-cap.release()
-cv2.destroyAllWindows()
+            if len(bboxs) == 0:
+                faces_number -= 1
+
+                Process(target=play_sound, args=("lost", lost_duration, )).start()
+
+            else:
+
+                for f in bboxs[faces_number - 1:]:
+    
+                    if f["score"] > min_score:
+                        faces_number -= 1
+
+                        Process(target=play_sound, args=("lost", lost_duration, )).start()
+
+        cv2.imshow("Image", img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
